2bc2459afe02c77e033f950064756947c84192a3
Yasmina Rahmani <creawesomeness.lab@gmail.com>
2020-10-26 14:49:23 +0100

    modif1

--- src/test/java/yannick/AppTest.java
+++ src/test/java/yannick/AppTest.java
@@ -10,9 +10,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AppTest {
     /**
      * Rigorous Test.
+     * 
+     * @return
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    /*
+     * void testApp() { assertEquals(1, 1);
+     */
+    public void name() {
+        assertEquals("Hello Yas", App.name("Yaz"),"Hello Yaz");
+
+        /*void hello() {
+		assertEquals("Hello World", Series1.helloWorld(null), "null name");*/
+
+
+
     }
+    
+
 }
+
+
