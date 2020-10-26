be364ca6f9b348d1d96299208a08e70f94362c3a
aurele3000 <aurele.hurtis@gmail.com>
2020-10-26 14:28:04 +0100

    Modif 2

--- src/main/java/yannick/Java/App.java
+++ src/main/java/yannick/Java/App.java
@@ -5,13 +5,17 @@ package yannick.Java;
  */
 public final class App {
     private App() {
-    }
+    //}
 
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println("Aurèle3000");
     }
-}
+}*/
+     public static String name(String name) {
+         return "Hello " + name;
+     }
+    }
--- src/test/java/yannick/AppTest.java
+++ src/test/java/yannick/AppTest.java
@@ -2,6 +2,8 @@ package yannick;
 
 import org.junit.jupiter.api.Test;
 
+import yannick.Java.App;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -12,7 +14,8 @@ class AppTest {
      * Rigorous Test.
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    
+    public void name() {
+        assertEquals("Hello Aurèle3000", App.name("Aurèle3000"), "Aurèlename");
     }
 }
